402a5bebb319c0c646b39f7ec577361655e56192
kevohmwach <50871815+kevohmwach@users.noreply.github.com>
2025-11-01 23:59:28 +0300

    updated site.layout.blade

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -41,17 +41,17 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Scripts -->
-    @vite([
+    {{-- @vite([
 
         'resources/js/app.js',
         'resources/css/site.css',
         'resources/css/product.css',
 
-        ])
+        ]) --}}
     
 
-    {{-- <link href="{{ asset('assets/css/site.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/product.css') }}" rel="stylesheet"> --}}
+    <link href="{{ asset('assets/css/site.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/product.css') }}" rel="stylesheet">
 
 
 
