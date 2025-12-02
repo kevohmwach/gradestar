aef3cfc74efafda52ae538e3a86376e2c2029182
kevohmwach <50871815+kevohmwach@users.noreply.github.com>
2025-12-03 00:21:20 +0300

    removed lazyloading for iframe

--- resources/views/shop/show.blade.php
+++ resources/views/shop/show.blade.php
@@ -25,7 +25,7 @@
                     src="/preview/{{$data->id}}" 
                     frameborder="0" 
                     scrolling="auto"
-                    loading="lazy"
+                    {{-- loading="lazy" --}}
                     title="{{$data->prod_title}}" >
                     <!DOCTYPE html>
                     <html lang="en">
